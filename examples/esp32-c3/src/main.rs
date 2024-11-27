c8b4a62fbac30f277b0bce13276d60a453ddfbf5
Scott Gibb <scott.gibb@dyson.com>
2024-11-27 11:36:08 +0000

    added some changes

--- examples/esp32-c3/src/main.rs
+++ examples/esp32-c3/src/main.rs
@@ -10,7 +10,6 @@ use esp_hal::{
 };
 use tmag5273::types::{DeviceVersion, TMag5273Error};
 use tmag5273::TMag5273;
-use crate::traits::{BitFieldDeviceConfiguration, ByteFieldDeviceConfiguration};
 
 /// Simple Main Function to run the example
 /// This will set up the I2C bus, create a TMag5273 Sensor, print out some device stats, set up the device and then loop round getting some data
@@ -20,7 +19,7 @@ use crate::traits::{BitFieldDeviceConfiguration, ByteFieldDeviceConfiguration};
 fn main() -> ! {
     esp_println::logger::init_logger(log::LevelFilter::Info);
     let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
+    let system = SystemControl::new(peripherals.SYSTEM)
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
 
     let clocks = ClockControl::max(system.clock_control).freeze();
