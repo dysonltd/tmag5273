16bb4d052e9e3398e3f2d853ac15889797808ef6
Scott Gibb <scott.gibb@dyson.com>
2024-12-13 15:57:55 +0000

    code now builds

--- examples/esp32-c3/src/main.rs
+++ examples/esp32-c3/src/main.rs
@@ -1,15 +1,15 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::i2c::{I2c, SevenBitAddress};
-use esp_backtrace as _;
-use esp_hal::delay::Delay;
-use esp_hal::{
-    clock::ClockControl, gpio::Io, i2c::I2C, peripherals::Peripherals, prelude::*,
-    system::SystemControl,
-};
+use esp_hal::i2c::master::I2c;
 use tmag5273::types::{DeviceVersion, TMag5273Error};
 use tmag5273::TMag5273;
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    prelude::*,
+};
+use embedded_hal::i2c::I2c as I2C_HAL;
 
 /// Simple Main Function to run the example
 /// This will set up the I2C bus, create a TMag5273 Sensor, print out some device stats, set up the device and then loop round getting some data
@@ -18,21 +18,21 @@ use tmag5273::TMag5273;
 #[entry]
 fn main() -> ! {
     esp_println::logger::init_logger(log::LevelFilter::Info);
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        // Configure the CPU to run at the maximum frequency.
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+    let delay = Delay::new();
     esp_println::println!("Running 1 Basic Readings!");
     // Set up your I2C
-    let i2c = I2C::new(
+    let i2c = I2c::new(
         peripherals.I2C0,
-        io.pins.gpio10,
-        io.pins.gpio8,
-        400.kHz(),
-        &clocks,
-    );
+        esp_hal::i2c::master::Config::default(),
+    )
+    .with_sda(peripherals.GPIO10)
+    .with_scl(peripherals.GPIO8);
 
     let mut mag_sensor = TMag5273::new(i2c, DeviceVersion::TMAG5273B1)
         .unwrap()
@@ -52,7 +52,7 @@ fn main() -> ! {
 /// Print out some device starts by reading the Device ID and Manufacturer ID, panic if it cant be done
 fn print_device_stats<I2C>(mag_sensor: &mut TMag5273<I2C>) -> Result<(), TMag5273Error>
 where
-    I2C: I2c<SevenBitAddress>,
+    I2C: I2C_HAL,
 {
     let device_id = mag_sensor.get_device_id()?;
     esp_println::println!("Device ID: {:?}", device_id);
