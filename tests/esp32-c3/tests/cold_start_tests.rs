cb5356ec1fa09042b5c86358ba1ed4ea8ba28057
Scott Gibb <scott.gibb@dyson.com>
2024-12-19 09:48:40 +0000

    Attempting to pass i2c

--- tests/esp32-c3/tests/cold_start_tests.rs
+++ tests/esp32-c3/tests/cold_start_tests.rs
@@ -1,26 +1,33 @@
 #![no_std]
 #![no_main]
 
-/// Sets up the logging before entering the test-body, so that embedded-test internal logs (e.g. Running Test <...>)  can also be printed.
-/// Note: you can also inline this method in the attribute. e.g. `#[embedded_test::tests(setup=rtt_target::rtt_init_log!())]`
-fn setup_log() {
-    #[cfg(feature = "log")]
-    rtt_target::rtt_init_log!();
-    #[cfg(feature = "defmt")]
-    rtt_target::rtt_init_defmt!();
-}
-
 #[cfg(test)]
 #[embedded_test::tests]
 mod tests {
-
+    use super::*;
+    use embedded_hal::i2c::I2c as I2C_HAL;
     #[init]
-    fn init() {
-        // This function is called once before all tests
+    fn init() -> I2C_HAL {
+        esp_println::logger::init_logger(log::LevelFilter::Info);
+        let peripherals = esp_hal::init({
+            let mut config = esp_hal::Config::default();
+            // Configure the CPU to run at the maximum frequency.
+            config.cpu_clock = CpuClock::max();
+            config
+        });
+        let delay = Delay::new();
+        esp_println::println!("Running 1 Basic Readings!");
+        // Set up your I2C
+        let i2c = I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default())
+            .with_sda(peripherals.GPIO5)
+            .with_scl(peripherals.GPIO6);
+
+        // The init function can return some state, which can be consumed by the testcases
+        i2c
     }
 
     #[test]
-    fn always_passes() {
+    fn always_passes(_state: I2C_HAL) {
         assert!(true);
     }
 }
