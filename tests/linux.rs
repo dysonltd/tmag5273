47e7b8486b06fd35711c4bb95f4c121fb5ef8530
Scott Gibb <scott.gibb@dyson.com>
2025-01-10 10:50:45 +0000

    removed unecessary comments

--- tests/linux.rs
+++ tests/linux.rs
@@ -2,7 +2,6 @@ use tests_common as common;
 
 #[cfg(test)]
 pub mod linux {
-    // Reference the correct path to the test files
     pub mod cold_start_tests;
     pub mod setting_registers_tests;
 }
