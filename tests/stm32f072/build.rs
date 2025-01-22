a41e015beea1b7de74fb874d7691dbb51a2cace3
Scott Gibb <scott.gibb@dyson.com>
2025-01-22 16:03:33 +0000

    Added working deps settings

--- tests/stm32f072/build.rs
+++ tests/stm32f072/build.rs
@@ -1,6 +1,8 @@
 fn main() {
-    // add linker script for embedded-test!!
+    // // stm32 specific
+    println!("cargo:rustc-link-arg=-Tlink.x");
+    // // add linker script for embedded-test!!
     println!("cargo::rustc-link-arg-tests=-Tembedded-test.x");
-    //link the defmt linker script
+    // //link the defmt linker script
     println!("cargo:rustc-link-arg=-Tdefmt.x");
 }
