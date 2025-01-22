821a9e82d4f046b4fac171e0d84f565645c8b2cd
Scott Gibb <scott.gibb@dyson.com>
2025-01-22 13:13:27 +0000

    updating to match new embassy hal

--- tests/stm32f072/src/lib.rs
+++ tests/stm32f072/src/lib.rs
@@ -3,30 +3,20 @@
 pub mod initialise {
     use embassy_stm32::{
         bind_interrupts,
-        dma::NoDma,
         i2c::{self, I2c},
+        mode::Blocking,
         peripherals,
         time::Hertz,
         Config,
     };
-    pub type Stm32I2c =
-        embassy_stm32::i2c::I2c<'static, embassy_stm32::peripherals::I2C1, NoDma, NoDma>;
+    pub type Stm32I2c = embassy_stm32::i2c::I2c<'static, Blocking>;
     pub fn initialise() -> Stm32I2c {
         bind_interrupts!(struct Irqs {
             I2C1 => i2c::EventInterruptHandler<peripherals::I2C1>, i2c::ErrorInterruptHandler<peripherals::I2C1>;
         });
         let config = Config::default();
         let p = embassy_stm32::init(config);
-        let i2c = I2c::new(
-            p.I2C1,
-            p.PB8,
-            p.PB9,
-            Irqs,
-            NoDma,
-            NoDma,
-            Hertz(400_000),
-            Default::default(),
-        );
+        let i2c = I2c::new_blocking(p.I2C1, p.PB8, p.PB9, Hertz(400_000), Default::default());
         i2c
     }
 }
