93a3d40b01bb17fc8e580d452d625b5060db1e1f
Scott Gibb <scott.gibb@dyson.com>
2025-01-23 13:29:56 +0000

    removed extra comments

--- tests/stm32f072/build.rs
+++ tests/stm32f072/build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    // // stm32 specific
+    // stm32 specific
     println!("cargo:rustc-link-arg=-Tlink.x");
-    // // add linker script for embedded-test!!
+    // add linker script for embedded-test!!
     println!("cargo::rustc-link-arg-tests=-Tembedded-test.x");
-    // //link the defmt linker script
+    //link the defmt linker script
     println!("cargo:rustc-link-arg=-Tdefmt.x");
 }
