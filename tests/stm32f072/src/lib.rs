65a736399bc8f11d313a84982c07388bd48d9bae
Scott Gibb <smgibb@yahoo.com>
2025-09-07 14:38:28 +0100

    refactor: Added updated stm32 tests for new api

--- tests/stm32f072/src/lib.rs
+++ tests/stm32f072/src/lib.rs
@@ -1,11 +1,14 @@
 #![no_std]
 /// This module is a simple setup method and type declarations that can be shared across the tests, reducing boiler plate code.
 pub mod initialise {
-    use embassy_stm32::{i2c::I2c, mode::Blocking, time::Hertz};
-    pub type Stm32I2c = embassy_stm32::i2c::I2c<'static, Blocking>;
+    use embassy_stm32::{
+        i2c::{I2c, Master},
+        mode::Blocking,
+    };
+    pub type Stm32I2c = embassy_stm32::i2c::I2c<'static, Blocking, Master>;
     pub fn initialise() -> Stm32I2c {
         let p = embassy_stm32::init(Default::default());
-        let i2c = I2c::new_blocking(p.I2C1, p.PB8, p.PB9, Hertz(400_000), Default::default());
+        let i2c = I2c::new_blocking(p.I2C1, p.PB8, p.PB9, Default::default());
         i2c
     }
 }
